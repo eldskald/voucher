e9704bf3861c614643639e108e5ea33c3e3103d1
rafael <rafaelbordoni00@gmail.com>
2022-09-22 20:39:10 -0300

    Update tests

--- tests/unit/serviceTests.test.ts
+++ tests/unit/serviceTests.test.ts
@@ -51,8 +51,8 @@ describe('Testing voucher services...', () => {
         discount: voucher.discount
       }
     });
-    const amount = Math.ceil(Math.random() * 100);
-    const finalAmount = amount;
+    const amount = 100 + Math.ceil(Math.random() * 100);
+    const finalAmount = amount * (1 - voucher.discount / 100);
     const results = await services.applyVoucher(voucher.code, amount);
     const usedVoucher = await prisma.voucher.findUnique({
       where: { code: voucher.code }
@@ -62,6 +62,27 @@ describe('Testing voucher services...', () => {
       discount: voucher.discount,
       finalAmount,
       applied: finalAmount !== amount
+    });
+    expect(usedVoucher.used).toBe(true);
+  });
+  it('Applying voucher to a less than 100 amount...', async() => {
+    const voucher = newVoucherFactory();
+    await prisma.voucher.create({
+      data: {
+        code: voucher.code,
+        discount: voucher.discount
+      }
+    });
+    const amount = Math.ceil(Math.random() * 100);
+    const results = await services.applyVoucher(voucher.code, amount);
+    const usedVoucher = await prisma.voucher.findUnique({
+      where: { code: voucher.code }
+    });
+    expect(results).toEqual({
+      amount,
+      discount: voucher.discount,
+      finalAmount: amount,
+      applied: false
     });
     expect(usedVoucher.used).toBe(false);
   });
